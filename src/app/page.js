44484d94f8dd4011ff9286009030f03a495fb165
[shashikant] <[shashikant.mane801@gmail.com]>
2024-12-26 16:27:20 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import ComingSonn from "@/components/frontend/CommingSoon";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <ComingSonn/>
+    </main>
+  );
+}
